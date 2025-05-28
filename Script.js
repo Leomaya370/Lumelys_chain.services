1947581da91f48956ea83a485927356d6b186c12
Leomaya370 <leoperez7808@gmail.com>
2025-05-28 19:41:44 -0400

    Update Script.js
    Signed-off-by: Leomaya370 <leoperez7808@gmail.com>

--- Script.js
+++ Script.js
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 // Conectar con Supabase
 const supabaseUrl = "https:// Leomaya370's Project.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6Z215Z2doa2p5em9yYWV6eHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0OTE1NTEsImV4cCI6MjA2MTA2NzU1MX0.NBZ2VcsfhqFQ2kyxpHgXQDNvCGE2Fe42Fw6RyA4m9qs";
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient( https://szgmygghkjyzoraezxrp.supabase.co,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6Z215Z2doa2p5em9yYWV6eHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0OTE1NTEsImV4cCI6MjA2MTA2NzU1MX0.NBZ2VcsfhqFQ2kyxpHgXQDNvCGE2Fe42Fw6RyA4m9qs );
 
 // Obtener datos del usuario y mostrarlos en la página
 async function obtenerDatos(usuario) {
