26c44c875e1be940e2c2792fe0ca7497c01262e5
Leomaya370 <leoperez7808@gmail.com>
2025-05-29 07:51:38 -0400

    Update Script.js
    Signed-off-by: Leomaya370 <leoperez7808@gmail.com>

--- Script.js
+++ Script.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Conectar con Supabase
-const supabaseUrl = "https://szgmygghkjyzoraezxrp.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6Z215Z2doa2p5em9yYWV6eHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0OTE1NTEsImV4cCI6MjA2MTA2NzU1MX0.NBZ2VcsfhqFQ2kyxpHgXQDNvCGE2Fe42Fw6RyA4m9qs";
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseUrl = "https://ccelrkpkzngfxdtfxugf.supabase.co";
+const supabaseAnonKey = "TU_API_KEY_AQUI";
+const supabase = supabase.createClient(supabaseUrl, supabaseAnonKey);
 
 // Función para obtener datos del usuario desde Supabase
 async function obtenerDatos(usuario) {
